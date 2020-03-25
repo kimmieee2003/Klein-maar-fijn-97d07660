89bcff30362d07599c05c1e8b427afcdbcfe9a2e
Kimberly Hogeveen <2103405@talnet.nl>
2020-03-25 15:06:19 +0100

    verbeter

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,17 +1,17 @@
 <?php
 $bedrag = readline(" ");
 $geldEnheden = array(50,20,10,5,2,1);
-$centen = array(50, 20, 10, 5, 2, 1);
+$centen = array(50, 20, 10, 5);
 $geld = intval($bedrag);
 $cent = $bedrag - $geld;
 $cent = intval(round($cent * 100));
-
+$cent = round($cent / 5) * 5;
 
 euro($geldEnheden, $geld);
 function euro($geldEnheden, $geld){
     foreach ($geldEnheden as $value) {
         $restgeld = floor($geld / $value);
-        if ($restgeld >= 1){
+        if ($restgeld >= 1) {
             $geld = $geld - ($value * $restgeld);
             echo $restgeld . "x" . $value . "euro" . PHP_EOL;
         }
@@ -21,7 +21,7 @@ centen($centen, $cent);
 function centen($centen, $cent){
     foreach ($centen as $value) {
         $restcent = floor($cent / $value);
-        if ($restcent >= 1){
+        if ($restcent >= 1) {
             $cent = $cent - ($value * $restcent);
             echo $restcent . " x" . $value . "cent" . PHP_EOL;
         }
